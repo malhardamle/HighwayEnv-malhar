10929066560d56bafe5bebae4113b452ec38bf56
Malhar Damle <mmdamle03@gmail.com>
2023-09-27 00:16:15 -0500

    Working manual dataset test script for EMG Presences / NO EMG Presence

--- scripts/model_training2.py
+++ scripts/model_training2.py
@@ -7,7 +7,7 @@ import sys
 import os 
 from sklearn.ensemble import RandomForestClassifier
 import gymnasium as gym
-import glob
+import glob, time
 
 obs_data = []
 emg_presence = []
@@ -25,9 +25,7 @@ def load_files(file_path):
                     else:
                         emg_presence.append(0)
                
-    print(len(obs_data), len(emg_presence), f)
-    for i in range(5):
-        print(emg_presence[i])
+    print("Total input datapoints "+ str(len(obs_data)) + " || total output label datapoints " + str(len(emg_presence))+ " || # of files:" + str(f))
     return obs_data, emg_presence
 
 def clean_model_input(obs_data, emg_presence):
@@ -45,7 +43,6 @@ def trainer(obs, emg):
     assert len(obs.shape) == 2 #cehck if obs is a 2d matrix
     assert len(emg.shape) == 1 #check if emg is 1d matrix
     final_model = RandomForestClassifier(n_estimators=200) #train model 
-    
     final_model.fit(obs,emg) # obs = 
     return final_model
 
@@ -103,52 +100,65 @@ def test_env(model):
 
 #get an accuracy count by running the model against 
 def accuracy_check(model):
-        f = 0
+        c = 0
         obs = []
        #open test episodes and extract obs data
         file_path = os.getcwd() + "/training_data/test/"
         for x in os.listdir(file_path):
             if x.endswith(".npy"):
-                f+=1
+                c+=1
                 data = np.load(os.path.join(file_path, x), allow_pickle=True)
                 obs.extend([d['obs'] for d in data])
-                print(len(obs),f) #check # of files and length of all obs data
-        print(predict(model,obs[0]))
-        # emg_total = 0  #total emg data points
-        # no_emg_total = 0 #total no emg data points
-        # emg_correct = 0 #correct emg predictions
-        # no_emg_correct = 0 #correct no emg predictions
-        # emg_presence = False #Ground truth
-        # for x in obs:  #accuracy calculations
-        #     if x[0] == 1: #emg vehicle is present 
-        #         emg_presence = True
-        #         emg_total +=1 #increment total emg count
-        #     else:
-        #         emg_presence = False
-        #         no_emg_total +=1
-        #     val = predict(model, x) #get model's prediction
-        #     if(emg_presence and val == 1):
-        #         emg_correct+= 1
-        #     elif(emg_presence == False and val==0):
-        #         no_emg_correct+=1
+    
+        count=0
+        emg_total = 0  #total emg data points
+        no_emg_total = 0 #total no emg data points
+        emg_correct = 0 #correct emg predictions
+        no_emg_correct = 0 #correct no emg predictions
+        f = open("test.txt", "w+")
+        for n in obs: #go thru all obs data in x_test
+            #3 row list
+            for i in n: #each row of 3row list
+                count +=1
+                data = i
+                x_test = data[None]
+                v = predict(model, x_test)
+                v = v[0]
+                f.write("Model Prediction: "+str(v)+ ", ")
+                emg_presence = False #Ground truth
+                if(data[0]==1):
+                    emg_presence = True
+                    f.write("GT: 1" +"\n")
+                    emg_total+=1
+                else:
+                    emg_presence = False
+                    f.write("GT: 0" +"\n")
+                    no_emg_total+=1
 
-        # #print accuracy results 
-        # emg_acc = (emg_correct / emg_total) * 100
-        # no_emg_acc = (no_emg_correct / no_emg_total) * 100
-        # print("EMG accuracy:" + str(emg_acc) + " No emg Accuracy:" + str(no_emg_acc))
+                if(emg_presence and v == 1): #emg exists and model predicts an emg presence
+                    emg_correct+= 1
+                elif(emg_presence == False and v==0): #no emg and model predicts 
+                    no_emg_correct+=1
 
+        #print accuracy results 
+        emg_acc = (emg_correct / emg_total) * 100
+        no_emg_acc = (no_emg_correct / no_emg_total) * 100
+
+        print("EMG accuracy:" + str(emg_acc) + " || No emg Accuracy:" + str(no_emg_acc))
+        print("Total EMG data points: " + str(emg_total) + " || Total No EMG Data points:" + str(no_emg_total))
+        print("# of Test datapoints:" + str(len(obs)*3) +  " count of datapoints:" + str(count) + " || number of test files:" + str(c))
 if __name__ == '__main__':
+    start_time = time.time()
     cwd = os.getcwd()
     cwd = cwd + "/training_data/emg_vehicle/"
     obs_data, action_data = load_files(cwd)
     cwd = os.getcwd() +'/training_data/no_emg/'
     obs_data, action_data = load_files(cwd)
     obs_data, action_data = clean_model_input(obs_data, action_data)
-
     model = trainer(obs_data, action_data)  #train model based on obs and action val data
-    print("Trained Model: ", model) #print weights of model
     important_features(model) #extract and print important features of model 
     #test_env(model)
     accuracy_check(model)
     
-    
+    elapsed_time = time.time() - start_time
+    print(f"Elapsed time to run the script: {elapsed_time:.3f} seconds")
