3b3368a8ee3fc8a1ef404b2b266c87c02867852c
Malhar Damle <mmdamle03@gmail.com>
2023-09-28 00:44:06 -0500

    K Fold Validation implemented

--- scripts/k_fold.py
+++ scripts/k_fold.py
@@ -1,3 +1,4 @@
+
 #########################################################################
 # Author: Malhar Damle
 # Description: Demonstrated Learning to update vehicle's action
@@ -9,7 +10,6 @@ from sklearn.ensemble import RandomForestClassifier
 import gymnasium as gym
 import glob, time
 from sklearn.model_selection import KFold
-from datetime import timedelta 
 
 
 obs_data = []
@@ -40,6 +40,7 @@ def clean_model_input(obs_data, emg_presence):
     obs_data = obs_data.reshape((batch * 3, -1)) # b*3 -> process one array (1 vehicles 6 features at once)
     # obs_data = obs_data.reshape((batch, -1)) #b *18 -> process entire batch -> 3 vehicles at a time with all 6 features each
     return obs_data, emg_presence
+
     
 def trainer(obs, emg):
     assert obs.shape[0] == emg.shape[0] #check if the obs and output label shape match
@@ -48,6 +49,7 @@ def trainer(obs, emg):
     final_model = RandomForestClassifier(n_estimators=200) #train model 
     final_model.fit(obs,emg) # obs = 
     return final_model
+
 
 def important_features(model):
      # Extract important features from the model
@@ -65,60 +67,74 @@ def important_features(model):
         print(f"{feature}: {importance:.4f}")
 
 
-
 def predict(final_model, x_test):
     predict_action = final_model.predict(x_test)
     return predict_action
 
-#get an accuracy count by running the model against 
-def accuracy_check(model):
-        c = 0
-        obs = []
-       #open test episodes and extract obs data
-        file_path = os.getcwd() + "/training_data/test/"
-        for x in os.listdir(file_path):
-            if x.endswith(".npy"):
-                c+=1
-                data = np.load(os.path.join(file_path, x), allow_pickle=True)
-                obs.extend([d['obs'] for d in data])
+def accuracy_calulation(model, test_data, x):
+     #running prediction on test data 
+    emg_total = 0  #total emg data points
+    no_emg_total = 0 #total no emg data points
+    emg_correct = 0 #correct emg predictions
+    no_emg_correct = 0 #correct no emg predictions
+    f = open(f"Fold:{x}.txt", "w+")
+    for i in test_data:
+        data = i
+        x_test = data[None]
+        v = predict(model, x_test)
+        v = v[0]
+        f.write("Model Prediction: "+str(v)+ ", ")
+        emg_presence = False #Ground truth
+        if(data[0]==1):
+            emg_presence = True
+            f.write("GT: 1" +"\n")
+            emg_total+=1
+        else:
+            emg_presence = False
+            f.write("GT: 0" +"\n")
+            no_emg_total+=1
+
+        if(emg_presence and v == 1): #emg exists and model predicts an emg presence
+            emg_correct+= 1
+        elif(emg_presence == False and v==0): #no emg and model predicts 
+            no_emg_correct+=1
+    #print accuracy results 
+    emg_acc = (emg_correct / emg_total) * 100
+    no_emg_acc = (no_emg_correct / no_emg_total) * 100
+    print(f"Fold {x} Accuracy Results:")
+    print("EMG accuracy: " + str(emg_acc) + " || No Emg Accuracy: " + str(no_emg_acc))
+    print("Total Test datapoints: " + str(len(test_data))+ " || Total EMG data points: " + str(emg_total) + " || Total No EMG Data points:" + str(no_emg_total))
     
-        count=0
-        emg_total = 0  #total emg data points
-        no_emg_total = 0 #total no emg data points
-        emg_correct = 0 #correct emg predictions
-        no_emg_correct = 0 #correct no emg predictions
-        f = open("test.txt", "w+")
-        for n in obs: #go thru all obs data in x_test
-            #3 row list
-            for i in n: #each row of 3row list
-                count +=1
-                data = i
-                x_test = data[None]
-                v = predict(model, x_test)
-                v = v[0]
-                f.write("Model Prediction: "+str(v)+ ", ")
-                emg_presence = False #Ground truth
-                if(data[0]==1):
-                    emg_presence = True
-                    f.write("GT: 1" +"\n")
-                    emg_total+=1
-                else:
-                    emg_presence = False
-                    f.write("GT: 0" +"\n")
-                    no_emg_total+=1
 
-                if(emg_presence and v == 1): #emg exists and model predicts an emg presence
-                    emg_correct+= 1
-                elif(emg_presence == False and v==0): #no emg and model predicts 
-                    no_emg_correct+=1
+def k_validation(dataset):
+    kf = KFold(n_splits=3, random_state=None,shuffle=True)
+    kf.get_n_splits(dataset)
+    print(kf)
+   
+    for i, (train_index, test_index) in enumerate(kf.split(dataset)): # go through each fold in kfold split
+        print("---------------------------------------------------------------------------")
+        print(f"Fold {i}:")
+        print("Total datapoints:",len(obs_data),"|| # of training data:", len(train_index), "|| # of test data:", len(test_index))
+        #print(f"  Train: index={train_index}") #list train index
+        train_data = []
+        emg_presence = []
+        for x in train_index:
+            train_data.append(obs_data[x])
+            if(obs_data[x][0] == 1):
+                emg_presence.append(1)
+            else:
+                emg_presence.append(0)
+        #print(f"  Test:  index={test_index}")
+        test_data = []
+        for x in test_index:
+            test_data.append(obs_data[x])       
+        train_data = np.array(train_data)
+        emg_presence = np.array(emg_presence)
 
-        #print accuracy results 
-        emg_acc = (emg_correct / emg_total) * 100
-        no_emg_acc = (no_emg_correct / no_emg_total) * 100
-
-        print("EMG accuracy:" + str(emg_acc) + " || No emg Accuracy:" + str(no_emg_acc))
-        print("Total EMG data points: " + str(emg_total) + " || Total No EMG Data points:" + str(no_emg_total))
-        print("# of Test datapoints:" + str(len(obs)*3) +  " count of datapoints:" + str(count) + " || number of test files:" + str(c))
+        model = trainer(train_data,emg_presence)
+        important_features(model)
+        accuracy_calulation(model,test_data, i)
+       
 
 if __name__ == '__main__':
     start_time = time.time()
@@ -126,12 +142,11 @@ if __name__ == '__main__':
     cwd = cwd + "/training_data/emg_vehicle/"
     obs_data, action_data = load_files(cwd)
     cwd = os.getcwd() +'/training_data/no_emg/'
-    obs_data, action_data = load_files(cwd)
-    obs_data, action_data = clean_model_input(obs_data, action_data)
-    model = trainer(obs_data, action_data)  #train model based on obs and action val data
-    important_features(model) #extract and print important features of model  
-    accuracy_check(model)
+    obs_data, action_data = load_files(cwd) # load all obs / action data
+    obs_data, action_data = clean_model_input(obs_data, action_data) #reformat data
+
+    k_validation(obs_data)
     
-    elapsed_time = time.time() - start_time 
+    elapsed_time = time.time() - start_time
     elapsed_time2 = elapsed_time/60
     print(f"Elapsed time to run the script: {elapsed_time:.3f} seconds or {elapsed_time2:.2f} minutes" )
